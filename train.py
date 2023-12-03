dafdf929cd8638d186f33c46c8504f8c030c0e17
Anoop CA <anoopafter@gmail.com>
2023-12-03 20:24:25 +0530

    Training experiments

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ torch.cuda.set_device(0)
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO(r'D:\ML_Projects\YOLOv8_Custom_Dataset_Pothole_Detection\yolov8x.pt')
+model = YOLO(r'D:\ML_Projects\YOLOv8_Custom_Dataset_Pothole_Detection\yolov8m.pt')
 
 # train the model
 if __name__ == '__main__':
-    results = model.train(data="data.yaml", epochs=10)
+    results = model.train(data="data.yaml", epochs=100)
